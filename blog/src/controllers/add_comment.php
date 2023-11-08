224a85db1bc55e8b36bc517ee4ef5f2daa2e1b1c
Hugo DUCRET <hd480557@iut21.u-bourgogne.fr>
2023-11-08 11:44:23 +0100

    Tirez parti de la composition

--- blog/src/controllers/add_comment.php
+++ blog/src/controllers/add_comment.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once('src/model/comment.php');
+require_once('blog/src/lib/database.php');
+require_once('blog/src/model/comment.php');
 
 function addComment(string $post, array $input)
 {
--- blog/src/controllers/post.php
+++ blog/src/controllers/post.php
@@ -1,11 +1,13 @@
 <?php
 
-require_once('src/model/post.php');
-require_once('src/model/comment.php');
+require_once('blog/src/lib/database.php');
+require_once('blog/src/model/post.php');
+require_once('blog/src/model/comment.php');
 
 function post(string $identifier)
 {
     $postRepository = new PostRepository();
+    $postRepository->connection = new DatabaseConnection();
     $post = $postRepository->getPost($identifier);
     $comments = getComments($identifier);
 
